8c2ff481acc3e3e67afd8e71486053fc83461dac
LanderU <lander.usategui@gmail.com>
2019-05-26 12:01:54 +0200

    Update some files to enable again OS X on travis:
    * Travis.yml: Enable OS X
    * .travis.osx.env: Change ros version to dashing
    * osx-install-ros2-binary.sh: Update to current release created by @ahcorde

--- osx-install-ros2-binary.sh
+++ osx-install-ros2-binary.sh
@@ -6,17 +6,17 @@ set -e #exit on failure
 #Prepare for ros2
 export dir=$(PWD)
 cd /tmp
-wget https://github.com/ros2/ros2/releases/download/release-crystal-20190408/ros2-crystal-20190408-macos-amd64.tar.bz2
-wget https://github.com/AcutronicRobotics/moveit_msgs/releases/download/crystal_patch4/moveit_msgs_crystal_patch4.zip
-wget https://github.com/AcutronicRobotics/object_recognition_msgs/releases/download/crystal_patch4/object_recognition_msgs_crystal_patch4.zip
-wget https://github.com/AcutronicRobotics/octomap_msgs/releases/download/crystal_patch4/octomap_msgs_crystal_patch4.zip
-tar jxf ros2-crystal-20190408-macos-amd64.tar.bz2
-unzip moveit_msgs_crystal_patch4.zip -d moveit_msgs_crystal_patch4
-unzip object_recognition_msgs_crystal_patch4.zip -d object_recognition_msgs_crystal_patch4
-unzip octomap_msgs_crystal_patch4.zip -d octomap_msgs_crystal_patch4
-cp -r moveit_msgs_crystal_patch4/* ros2-osx/
-cp -r object_recognition_msgs_crystal_patch4/* ros2-osx/
-cp -r octomap_msgs_crystal_patch4/* ros2-osx/
+wget https://github.com/AcutronicRobotics/moveit2/releases/download/ci_osx_dashing/ros2_osx.zip
+unzip -q ros2_osx.zip
+wget https://github.com/AcutronicRobotics/moveit_msgs/releases/download/dashing_pre-release/dashing_pre-release.zip -O moveit_msgs.zip
+unzip -q moveit_msgs.zip -d object_recognition_msgs
+wget https://github.com/AcutronicRobotics/octomap_msgs/releases/download/dashing_pre-release/dashing_pre-release.zip -O octomap_msgs
+unzip -q octomap_msgs.zip -d octomap_msgs
+wget https://github.com/AcutronicRobotics/object_recognition_msgs/releases/download/dashing_pre-release/dashing_pre-release.zip -O object_recognition_msgs.zip
+unzip -q object_recognition_msgs.zip -d object_recognition_msgs
+cp -r moveit_msgs/dashing_pre-release/* ros2-osx/
+cp -r object_recognition_msgs/dashing_pre-release/* ros2-osx/
+cp -r octomap_msgs/dashing_pre-release* ros2-osx/
 # Remove tf2_eigen
 find ros2-osx/ -name tf2_eigen | xargs rm -rf
 find ros2-osx/ -name resource_retriever | xargs rm -rf
